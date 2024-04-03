acafd31af735f3f2b8ac412d9a366fdc7e8fed90
Gregory Waxman <akkuma@users.noreply.github.com>
2024-04-03 19:57:14 -0400

    Attempt to publish page

---
+++ docs/_build/assets/index-uGQHsEaY.js
@@ -0,0 +1,1 @@
+import{i as t,t as e}from"./web-BWkzByzL.js";var a=e('<main class="text-center mx-auto text-gray-700 p-4"><h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Insert Imagination</h1><p class=mt-8>Nothing to see here, yet.');function m(){return t(a)}export{m as default};
