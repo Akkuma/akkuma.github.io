ecedf4a88aec75d06e270a62865846c5f67aeaf6
Gregory Waxman <akkuma@users.noreply.github.com>
2025-02-07 16:15:23 -0500

    Undo imports?

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import { Router } from '@solidjs/router';
 import { FileRoutes } from '@solidjs/start/router';
 import { Suspense } from 'solid-js';
-import Nav from '#/components/nav';
+import Nav from './components/nav';
 import './app.css';
 import { Box } from './components/box.jsx';
 
