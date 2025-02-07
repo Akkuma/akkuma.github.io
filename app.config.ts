c0179c18ccee68254f6deedd6d090c2b9194cf50
Gregory Waxman <akkuma@users.noreply.github.com>
2025-02-07 16:51:57 -0500

    Remove baseURL?

--- app.config.ts
+++ app.config.ts
@@ -7,7 +7,6 @@ const mdx = vMdx.default;
 export default defineConfig({
 	extensions: ['mdx'],
 	server: {
-		baseURL: process.env.BASE_PATH,
 		prerender: {
 			crawlLinks: true,
 			routes: ['/', '/404.html'],
