8580b3b3e4d985417f1c634cdf1965d7119e152a
Gregory Waxman <akkuma@users.noreply.github.com>
2025-03-07 13:29:00 -0500

    Fixed wording

--- src/components/recipes/csharp.tsx
+++ src/components/recipes/csharp.tsx
@@ -7,7 +7,7 @@ export function CSharp() {
 			<Card.Front
 				icon={<CSharpLogo />}
 				name="C#"
-				about="When you want an artisanally crafted OOP language artisanally by the legendary Anders Hejlsberg."
+				about="When you want an artisanally crafted OOP language by the legendary Anders Hejlsberg (see also TypeScript)."
 			/>
 			<Card.Back icon={<CSharpLogo />} name="C#" start="2010" prep="4 years" expertise="Mid" />
 		</Card>
