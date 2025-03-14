9d808f16292f178411f4fe2c74f115dd3b2128ce
Gregory Waxman <akkuma@users.noreply.github.com>
2025-03-14 09:57:23 -0400

    Post title colored and transitionable

--- src/blog/header.tsx
+++ src/blog/header.tsx
@@ -3,7 +3,7 @@ const dtf = new Intl.DateTimeFormat('en-US', { month: 'short', year: 'numeric', 
 export function Header(props: { title: string; date: Date }) {
 	return (
 		<header class="text-[2.25em] mb-[0.8888889em] flex flex-col">
-			<h1 class="!mb-0 !text-[1em]">{props.title}</h1>
+			<h1 class="!mb-0 !text-[1em] text-accent">{props.title}</h1>
 			<time class="text-xs" dateTime={props.date.toISOString()}>
 				{dtf.format(props.date)}
 			</time>
